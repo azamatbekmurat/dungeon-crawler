ec04b62b1f03c8c31f8ed47a20c3b419e64e8263
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-05-23 07:30:17 -0700

    add walking animation to player

--- src/features/player/movement.js
+++ src/features/player/movement.js
@@ -16,6 +16,24 @@ function handleMovement(player) {
     }
   }
 
+  function getSpriteLocation(direction, walkIndex) {
+    switch (direction) {
+      case "SOUTH":
+        return `${SPRITE_SIZE * walkIndex}px ${SPRITE_SIZE * 0}px`;
+      case "EAST":
+        return `${SPRITE_SIZE * walkIndex}px ${SPRITE_SIZE * 1}px`;
+      case "WEST":
+        return `${SPRITE_SIZE * walkIndex}px ${SPRITE_SIZE * 2}px`;
+      case "NORTH":
+        return `${SPRITE_SIZE * walkIndex}px ${SPRITE_SIZE * 3}px`;
+    }
+  }
+
+  function getWalkIndex() {
+    const walkIndex = store.getState().player.walkIndex;
+    return walkIndex >= 7 ? 0 : walkIndex + 1;
+  }
+
   function observeBoundaries(oldPos, newPos) {
     return (
       newPos[0] >= 0 &&
@@ -32,11 +50,15 @@ function handleMovement(player) {
     return nextTile < 5;
   }
 
-  function dispatchMove(newPos) {
+  function dispatchMove(direction, newPos) {
+    const walkIndex = getWalkIndex();
     store.dispatch({
       type: "MOVE_PLAYER",
       payload: {
-        position: newPos
+        position: newPos,
+        direction,
+        walkIndex,
+        spriteLocation: getSpriteLocation(direction, walkIndex)
       }
     });
   }
@@ -46,7 +68,7 @@ function handleMovement(player) {
     const newPos = getNewPosition(oldPos, direction);
 
     if (observeBoundaries(oldPos, newPos) && observeImpassible(oldPos, newPos))
-      dispatchMove(newPos);
+      dispatchMove(direction, newPos);
   }
 
   function handleKeyDown(e) {
