23c34041368f4d3ca33431074ee0eb557c038402
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-05-22 23:15:18 -0700

    add getBoundaries function in order to restrict movement of player out of the game map

--- src/features/player/movement.js
+++ src/features/player/movement.js
@@ -1,6 +1,6 @@
 import React from "react";
 import store from "../../config/store";
-import { SPRITE_SIZE } from "../../config/constants";
+import { SPRITE_SIZE, MAP_WIDTH, MAP_HEIGHT } from "../../config/constants";
 
 function handleMovement(player) {
   function getNewPosition(direction) {
@@ -16,11 +16,20 @@ function handleMovement(player) {
         return [oldPos[0], oldPos[1] + SPRITE_SIZE];
     }
   }
+
+  function observeBoundaries(oldPos, newPos) {
+    return newPos[0] >= 0 &&
+      newPos[0] <= MAP_WIDTH - SPRITE_SIZE &&
+      (newPos[1] >= 0 && newPos[1] <= MAP_HEIGHT - SPRITE_SIZE)
+      ? newPos
+      : oldPos;
+  }
   function dispatchMove(direction) {
+    const oldPos = store.getState().player.position;
     store.dispatch({
       type: "MOVE_PLAYER",
       payload: {
-        position: getNewPosition(direction)
+        position: observeBoundaries(oldPos, getNewPosition(direction))
       }
     });
   }
