6bda222695f2a6812ee9f99c447c80bbb90b9000
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2019-05-22 22:42:08 -0700

    add constant file with sprite size

--- src/features/player/index.js
+++ src/features/player/index.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import walkSprite from "./player_walk.png";
+import handleMovement from "./movement";
 
 function Player(props) {
-  return <div>Player</div>;
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: props.position[1],
+        left: props.position[0],
+        backgroundImage: `url('${walkSprite}')`,
+        backgroundPosition: "0 0",
+        width: "40px",
+        height: "40px"
+      }}
+    />
+  );
 }
 
-export default Player;
+function mapStateToProps(state) {
+  return {
+    ...state.player
+  };
+}
+
+export default connect(mapStateToProps)(handleMovement(Player));
